0a08958064abb2335db56c2bb2fad76675bff8d9
lin.zhao <lin.zhao@cloudwise.com>
2023-03-24 11:14:07 +0800

    feat:主页新增功能，主题色修改中

--- src/pages/NbHome/components/VtuberCard/index.vue
+++ src/pages/NbHome/components/VtuberCard/index.vue
@@ -1,5 +1,51 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import { vtuberDetailList } from '@/stores/vtuber'
+
+const sortMethod = ref('默认')
+
+const sortField = [
+  { label: '默认', value: '' },
+  { label: '总时长', value: 'totalLiveSecond' },
+  { label: '总场次', value: 'totalLiveCount' },
+  {
+    label: '总流水',
+    value: 'totalIncome'
+  },
+  { label: '平均流水', value: 'averageIncome' }
+]
+
+const sortedList = computed(() => {
+  switch (sortMethod.value) {
+  case 'totalLiveSecond':
+    return vtuberDetailList.value.sort((a, b) => {
+      return b.channel.totalLiveSecond - a.channel.totalLiveSecond
+    })
+  case 'totalLiveCount':
+    return vtuberDetailList.value.sort((a, b) => {
+      return b.channel.totalLiveCount - a.channel.totalLiveCount
+    })
+  case 'totalIncome':
+    return vtuberDetailList.value.sort((a, b) => {
+      return b.channel.totalIncome - a.channel.totalIncome
+    })
+  case 'averageIncome':
+    return vtuberDetailList.value.sort((a, b) => {
+      return (
+        b.channel.totalIncome / (b.channel.totalLiveSecond / (60 * 60)) -
+          a.channel.totalIncome / (a.channel.totalLiveSecond / (60 * 60))
+      )
+    })
+  default:
+    return vtuberDetailList.value
+  }
+})
+const jumpToDetail = () => {
+  console.log(1)
+}
+const jumpToChannel = () => {
+  console.log(2)
+}
 </script>
 
 <script lang="ts">
@@ -12,13 +58,31 @@ export default defineComponent({
 </script>
 
 <template>
+  <div class="vtuber-card-sort">
+    <div class="vtuber-card-sort-label">
+      排序方式：
+    </div>
+    <el-radio-group v-model="sortMethod">
+      <template
+        v-for="item in sortField"
+        :key="item.value"
+      >
+        <el-radio-button :label="item.value">
+          {{ item.label }}
+        </el-radio-button>
+      </template>
+    </el-radio-group>
+  </div>
   <div class="vtuber-card-wrap">
     <div
-      v-for="(item, index) in vtuberDetailList"
+      v-for="(item, index) in sortedList"
       :key="index"
       class="vtuber-card"
     >
-      <div class="vtuber-info">
+      <div
+        class="vtuber-info"
+        @click.prevent="jumpToDetail"
+      >
         <div>
           <div class="vtuber-avatar">
             <el-avatar
@@ -29,6 +93,7 @@ export default defineComponent({
             <div
               v-if="item.channel.isLiving"
               class="vtuber-info-stream"
+              @click.stop="jumpToChannel"
             >
               直播中
             </div>
@@ -61,6 +126,16 @@ export default defineComponent({
           :value="item.channel.totalIncome"
         />
       </div>
+      <div class="vtuber-card-divider" />
+      <div class="vtuber-data">
+        <el-statistic
+          title="平均流水（元/时）"
+          :value="
+            item.channel.totalIncome /
+              (item.channel.totalLiveSecond / (60 * 60))
+          "
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -69,8 +144,20 @@ export default defineComponent({
 @import '@/styles/variables.module.scss';
 
 $minWidth: 200px;
+$wrapWidth: 80%;
+
+.vtuber-card-sort {
+  width: $wrapWidth;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  .vtuber-card-sort-label{
+    font-size: 14px;
+  }
+}
+
 .vtuber-card-wrap {
-  width: 80%;
+  width: $wrapWidth;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -81,7 +168,7 @@ $minWidth: 200px;
     justify-content: space-around;
     align-items: center;
     box-sizing: border-box;
-    padding: 16px;
+    padding: 12px;
     margin-bottom: 32px;
     width: 100%;
     border-radius: 4px;
@@ -102,6 +189,7 @@ $minWidth: 200px;
   padding-right: 8px;
   min-width: $minWidth;
   text-align: center;
+  cursor: pointer;
 
   .vtuber-avatar {
     position: relative;
