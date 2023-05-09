9b2e150df23d01d770c043265dd5abcc1690e532
star66 <371132562@qq.com>
2023-05-09 21:38:04 +0800

    feat:尝试跨域

--- src/services/vtuber.js
+++ src/services/vtuber.js
@@ -2,5 +2,5 @@ import http from './base'
 
 //根据uid获取直播信息
 export const getChannelDetail = (params = {}) => {
-  return http.get(`https://api.ukamnads.icu/api/v2/channel`, { params })
+  return http.get(`/api.ukamnads.icu/api/v2/channel`, { params })
 }
