7cbe902b7f12223d46e86bdd5875f049fdda2d8a
star66 <371132562@qq.com>
2023-05-09 22:30:13 +0800

    Revert "feat:尝试跨域"
    This reverts commit 6b23c5cd9e1c20fe33073043f4f60aed3c396110.

--- api/vtuber.js
+++ api/vtuber.js
@@ -4,6 +4,3 @@ import http from '@/services/base'
 export const getChannelDetail = (params = {}) => {
   return http.get(`https://api.ukamnads.icu/api/v2/channel`, { params })
 }
-
-
-http.get('https://api.ukamnads.icu/api/v2/channel?uId=1011797664&includeExtra=true')
